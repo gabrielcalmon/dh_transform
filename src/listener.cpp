7f9138c2b77940fb965a5dcd61d7e4f901ee0af9
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-09-12 15:24:31 -0300

    implement conversion from Matrix4d to geometryPose

--- src/listener.cpp
+++ src/listener.cpp
@@ -6,6 +6,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/pose.hpp"
 
 using namespace std::chrono_literals;
 using Eigen::MatrixXd, Eigen::Matrix4d, Eigen::Vector4d, Eigen::VectorXd;
@@ -27,19 +28,20 @@ public:
 
         // THIS CODE WAS NOT SUPOSED TO BE ON CONSTRUCTOR
         if(number_of_links != int(dh_vector.size())){    // It is expected that the DH params come in multiples of 4
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "The number of links passed doesn't match the number of lenghts");
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                "The number of links passed doesn't match the number of lengths");
             // [TODO] THROW ERROR; INTERRUPT CODE
-        }    
+        }
         std::vector<double> initial_angles = std::vector<double>(number_of_links, 0);
         dh_matrix = calculateDhMatrix(dh_vector, initial_angles);
 
         int number_of_dh_rows = static_cast<int>(dh_matrix.rows());
-        
-        end_effector_pose = calculateEndEffectorPose(dh_matrix, number_of_dh_rows);
+
+        end_effector_pose = calculateEndEffectorTransform(dh_matrix, number_of_dh_rows);
 
         Eigen::Quaterniond end_effector_quaternion(end_effector_pose.block<3,3>(0,0));
 
-        std::cout << "End effector quarternion: " << end_effector_quaternion << std::endl;        
+        std::cout << "End effector quarternion: " << end_effector_quaternion << std::endl;
     }
 
 private:
@@ -53,7 +55,8 @@ private:
     double deg2rad(double deg);
     MatrixXd calculateDhMatrix(std::vector<double> link_lenghts, std::vector<double> angles);
     Matrix4d dh2Transform(Vector4d dh_line);
-    Matrix4d calculateEndEffectorPose(MatrixXd dh_matrix, int num_of_lines);
+    Matrix4d calculateEndEffectorTransform(MatrixXd dh_matrix, int num_of_lines);
+    geometry_msgs::msg::Pose endEffectorTransformToPose(Matrix4d end_effector_transform);
 };
 
 double DhCalculator::deg2rad(double deg){
@@ -69,7 +72,7 @@ MatrixXd DhCalculator::calculateDhMatrix(std::vector<double> link_lenghts, std::
     }
 
     MatrixXd dh_matrix(num_joints+1, 4);    // a dh matrix has 4 columns and (num_joints+1) rows
-    dh_matrix << MatrixXd::Zero(4,4);
+    dh_matrix << MatrixXd::Zero(num_joints+1,4);
     
     for(int i=0; i<=num_joints; i++){
         if(i == 0){     // frame 0 (base)
@@ -88,7 +91,7 @@ MatrixXd DhCalculator::calculateDhMatrix(std::vector<double> link_lenghts, std::
 
 Matrix4d DhCalculator::dh2Transform(Vector4d dh_line){
     double alfaRad = deg2rad(dh_line(0));
-    double a = dh_line(1);  // alpha
+    double a = dh_line(1);
     double d = dh_line(2);
     double thetaRad = deg2rad(dh_line(3));
     Matrix4d transform{
@@ -101,7 +104,7 @@ Matrix4d DhCalculator::dh2Transform(Vector4d dh_line){
     return transform;
 }
 
-Matrix4d DhCalculator::calculateEndEffectorPose(MatrixXd dh_matrix, int num_of_lines){
+Matrix4d DhCalculator::calculateEndEffectorTransform(MatrixXd dh_matrix, int num_of_lines){
     Matrix4d end_pose;
     end_pose << MatrixXd::Identity(4,4);
 
@@ -110,9 +113,19 @@ Matrix4d DhCalculator::calculateEndEffectorPose(MatrixXd dh_matrix, int num_of_l
         end_pose *= dh2Transform(dh_line);
     }
 
-    std::cout << "out put transform:\n" << end_pose <<std::endl;
+    std::cout << "output transform:\n" << end_pose <<std::endl;
 
     return end_pose;
+}
+
+geometry_msgs::msg::Pose DhCalculator::endEffectorTransformToPose(Matrix4d end_effector_transform){
+    geometry_msgs::msg::Pose end_effector_pose = geometry_msgs::msg::Pose();
+    Eigen::Quaterniond end_effector_quaternion(end_effector_transform.block<3,3>(0,0));
+    end_effector_pose.orientation.x = end_effector_quaternion.x();
+    end_effector_pose.orientation.y = end_effector_quaternion.y();
+    end_effector_pose.orientation.z = end_effector_quaternion.z();
+    end_effector_pose.orientation.w = end_effector_quaternion.w();
+    return end_effector_pose;
 }
 
 
