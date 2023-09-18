355f106c2923d5268e81e47b8ea2b69e6633a0c3
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-09-18 15:21:59 -0300

    [fix commit] implement config file as variable

--- launch/pose_publisher_launch.py
+++ launch/pose_publisher_launch.py
@@ -7,13 +7,14 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    manipulator_config_filename='manipulator_3l.yaml'
     return LaunchDescription([
         Node(
             package='dh_transform',
             executable='pose_publisher',
             parameters=[os.path.join(
                 get_package_share_directory('dh_transform'),
-                'config', 'manipulator_3l.yaml')],
+                'config', manipulator_config_filename)],
             output='screen'),
 
         Node(
@@ -21,7 +22,7 @@ def generate_launch_description():
             executable='encoder',
             parameters=[os.path.join(
                 get_package_share_directory('dh_transform'),
-                'config', 'manipulator_3l.yaml')],
+                'config', manipulator_config_filename)],
             output='screen'),
 
         Node(
