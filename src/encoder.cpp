be9c259792c8812e15cb374e5ab50ca0e35ac450
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-09-18 15:00:10 -0300

    add service to update joint angles

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -4,6 +4,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+#include "dh_transform/srv/angles_update.hpp"
 
 // This classes simulates a group of joint encoder's, publishing the angular postion
 class FakeEncoderPublisher : public rclcpp::Node {
@@ -15,8 +16,24 @@ public:
         timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&FakeEncoderPublisher::publish_sensor_values, this));
 
         fake_deg_values = std::vector<double>(num_joints, default_joints_value);
-        
-        // update_joint_values();
+
+        // Crie um serviço para receber atualizações do vetor de dados
+        update_angles_service_ = this->create_service<dh_transform::srv::AnglesUpdate>(
+            "update_angles", std::bind(&FakeEncoderPublisher::updateAnglesCallback, this, std::placeholders::_1, std::placeholders::_2));
+    }
+
+    // Service used to update joint angles in runtime
+    void updateAnglesCallback(
+        const std::shared_ptr<dh_transform::srv::AnglesUpdate::Request> request,
+        std::shared_ptr<dh_transform::srv::AnglesUpdate::Response> response)
+    {
+        fake_deg_values = request->angles;
+        RCLCPP_INFO(this->get_logger(), "Received array of angles: ");
+        for (const auto& value : fake_deg_values) {
+            RCLCPP_INFO(this->get_logger(), "%.2f", value);
+        }
+
+        response->success = true;
     }
 
 private:
@@ -34,24 +51,19 @@ private:
         publisher_->publish(std::move(joint_state_msg));
     }
 
-    void update_joint_values(){
-        std::cout << "Type " << num_joints << " angular values separated by space:" << std::endl;
-        for (int i = 0; i < num_joints; i++) {
-            std::cin >> fake_deg_values[i];
-        }
-    }
-
     const int num_joints;
     std::vector<double> fake_deg_values;
     double default_joints_value;     // initial angular position (in degrees) 
     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Service<dh_transform::srv::AnglesUpdate>::SharedPtr update_angles_service_;
 };
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<FakeEncoderPublisher>(5, 0);
+    auto node = std::make_shared<FakeEncoderPublisher>(3, 0);
     rclcpp::spin(node);
+
     rclcpp::shutdown();
     return 0;
 }
